5e08761aca428e5bfd19e2db142b7eff15f01b81
Ailton Duarte <adidas.coyas@gmail.com>
2021-01-31 01:18:07 -0100

    building block

--- projecto2.c
+++ projecto2.c
@@ -3,17 +3,40 @@
 
 int main() {
 	int total;
-	puts("##################################\n");
-	puts("Benvindo ao sistema de controle de alunos e notas\n");
-	puts("##################################\n");
-
-	puts("Quantos alunos:");
-	scanf("%d", total);
+	char nome[10], apelido[10], disciplina[10],nomeprof[10], apelidoprof[10];
 
 	int count = 0;
-	do {
+	float nota1, nota2;
 
-	}while(count < total)
+	while(1) {
+		// Apresentar o menu
+		printf("#######################################################\n");
+		printf("## Benvindo ao sistema de controle de alunos e notas ##\n");
+		printf("#################################################### ##\n");
+
+		// pegar os dados para serem processados
+		printf("Digite o nome e apelido do aluno: ");
+		scanf("%s %s", &nome, &apelido);
+		printf("Digite o nome e apelido do professor: ");
+		scanf("%s %s", &nomeprof, &apelidoprof);
+		printf("qual é a disciplina: ");
+		scanf("%s", &disciplina);	
+		printf("Nota teste 1: ");
+		scanf("%f", &nota1);
+		printf("Nota teste 2: ");
+		scanf("%f", &nota2);	
+
+		// Apresentar os calculos dos dados
+		printf("\n\n\n\n##################################\n");
+		printf("Resultados das notas\n");
+		printf("Nome: %s %s\n", nome, apelido);
+		printf("Professor: %s %s\n", nomeprof, apelidoprof);
+		printf("disciplina: %s\n", disciplina);
+		printf("teste 1: %2.2f\n", nota1);
+		printf("teste 2: %2.2f\n", nota2);
+		printf("Media: %d\n", (nota1 + nota2) / 2);
+		printf("##################################\n\n\n\n\n");
+	}
 
 
 }
