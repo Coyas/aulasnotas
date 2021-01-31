8305927d8804a50e29a0351f0c3bc2e9c265876f
Ailton Duarte <adidas.coyas@gmail.com>
2021-01-31 20:40:04 -0100

    change tree

--- src/projecto2.c
+++ src/projecto2.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+struct projecto2
+{
+	/* data */
+	char nome[10];
+	char apelido[10];
+	char nomeprof[10];
+	char apelidoprof[10];
+	char disciplina[30];
+	float nota1;
+	float nota2;
+};
+
+
 int main() {
 	int total;
 	char nome[10], apelido[10], disciplina[10],nomeprof[10], apelidoprof[10];
 
 	int count = 0;
-	float nota1, nota2;
+	
 
 	while(1) {
 		// Apresentar o menu
@@ -22,9 +35,9 @@ int main() {
 		printf("qual é a disciplina: ");
 		scanf("%s", &disciplina);	
 		printf("Nota teste 1: ");
-		scanf("%f", &nota1);
-		printf("Nota teste 2: ");
-		scanf("%f", &nota2);	
+		// scanf("%f", &nota1);
+		// printf("Nota teste 2: ");
+		// scanf("%f", &nota2);	
 
 		// Apresentar os calculos dos dados
 		printf("\n\n\n\n##################################\n");
@@ -32,9 +45,9 @@ int main() {
 		printf("Nome: %s %s\n", nome, apelido);
 		printf("Professor: %s %s\n", nomeprof, apelidoprof);
 		printf("disciplina: %s\n", disciplina);
-		printf("teste 1: %2.2f\n", nota1);
-		printf("teste 2: %2.2f\n", nota2);
-		printf("Media: %d\n", (nota1 + nota2) / 2);
+		// printf("teste 1: %2.2f\n", nota1);
+		// printf("teste 2: %2.2f\n", nota2);
+		// printf("Media: %d\n", (nota1 + nota2) / 2);
 		printf("##################################\n\n\n\n\n");
 	}
 
